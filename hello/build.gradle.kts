977443a2503a1fd32c17bf1f03190568dab7915a
bekurin <bekurin17@gmail.com>
2024-02-10 14:06:30 +0900

    feat: testcontainer 연동 및 기존 action 디렉토리 변경

--- hello/build.gradle.kts
+++ hello/build.gradle.kts
@@ -1,10 +1,16 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
+val testContainersVersion: String = "1.19.5"
+val mysqlConnectorVersion: String = "8.0.32"
+val mockitoKotlinVersion: String = "4.1.0"
+val okhttpVersion: String = "4.10.0"
+
 plugins {
     id("org.springframework.boot") version "3.2.2"
     id("io.spring.dependency-management") version "1.1.4"
     kotlin("jvm") version "1.9.22"
     kotlin("plugin.spring") version "1.9.22"
+    kotlin("plugin.jpa") version "1.9.22"
 }
 
 group = "com.example"
@@ -22,7 +28,20 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-data-redis")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+
+    // mockito-kotlin and mockwebserver
+    testImplementation("org.mockito.kotlin:mockito-kotlin:$mockitoKotlinVersion")
+    testImplementation("com.squareup.okhttp3:okhttp:$okhttpVersion")
+    testImplementation("com.squareup.okhttp3:mockwebserver:$okhttpVersion")
+
+    // testcontainers
+    testImplementation("org.testcontainers:testcontainers:$testContainersVersion")
+    testImplementation("org.testcontainers:junit-jupiter:$testContainersVersion")
+    testImplementation("org.testcontainers:mysql:$testContainersVersion")
+    testRuntimeOnly("mysql:mysql-connector-java:$mysqlConnectorVersion")
 }
 
 tasks.withType<KotlinCompile> {
--- hello/src/test/kotlin/com/example/hello/HelloApplicationTests.kt
+++ hello/src/test/kotlin/com/example/hello/HelloApplicationTests.kt
@@ -1,13 +1,14 @@
 package com.example.hello
 
+import com.example.hello.config.IntegrationTestBase
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
-import org.springframework.boot.test.context.SpringBootTest
 
-@SpringBootTest
-class HelloApplicationTests {
+class HelloApplicationTests : IntegrationTestBase() {
 
     @Test
     fun contextLoads() {
+        val one = 1
+        assertThat(one).isEqualTo(1)
     }
-
 }
