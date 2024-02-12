c0781c32a27d87b4e24d426ee71c00b58b054c28
bekurin <bekurin17@gmail.com>
2024-02-12 15:41:55 +0900

    fixed untracked files

---
+++ .github/workflows/echo/action.ts
@@ -0,0 +1,8 @@
+import core from "@actions/core";
+
+try {
+    const message = core.getInput("MESSAGE")
+    console.log(`echo input message: ${message}`)
+} catch (error) {
+    core.setFailed(error.message)
+}
