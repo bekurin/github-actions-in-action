82d5a1463590c63757d3b3a611c25f7e0669db72
bekurin <bekurin17@gmail.com>
2024-02-13 19:16:02 +0900

    feat: create review reminder actions

---
+++ .github/workflows/opened-pull-request/action.js
@@ -0,0 +1,20 @@
+const github = require("@actions/github")
+const core = require("@actions/core")
+
+try {
+    const owner = github.context.repo.owner;
+    const repo = github.context.repo.repo;
+
+    const githubClient = github.getOctokit(process.env.GITHUB_TOKEN);
+    const {data: pullRequests} = await githubClient.rest.pulls.list({
+        owner,
+        repo,
+        state: "open",
+        per_page: 100,
+        sort: "updated",
+        direction: "desc"
+    });
+    console.log(pullRequests)
+} catch (error) {
+    core.setFailed(error.message)
+}
